c38f7043a9bdb7cf28ba52d4d9ae0de02e4e5043
Helena Steck <helena.steck@vtex.com.br>
2020-01-08 18:34:33 -0300

    Added tests for integers and for booleans

--- src/hooks/__tests__/useRadio.test.tsx
+++ src/hooks/__tests__/useRadio.test.tsx
@@ -5,13 +5,29 @@ import { useRadio } from '../useRadio'
 import { FormContext } from '../../components'
 
 const mockRadioSchema = {
-  type: 'string',
-  name: 'test-useRadio',
-  enum: ['this', 'tests', 'the', 'useRadio', 'hook'],
+  type: 'object',
+  properties: {
+    stringTest: {
+      type: 'string',
+      name: 'test-useRadioString',
+      enum: ['this', 'tests', 'the', 'useRadio', 'hook'],
+    },
+    integerTest: {
+      type: 'integer',
+      name: 'test-useRadioInteger',
+      minimum: 0,
+      maximum: 10,
+      multipleOf: 1,
+    },
+    booleanTest: {
+      type: 'boolean',
+      name: 'test-useRadioBoolean',
+    },
+  },
 }
 
-const MockRadio: FC<{}> = () => {
-  const methods = useRadio('#/')
+const MockRadio: FC<{ path: string }> = props => {
+  const methods = useRadio(props.path)
 
   return (
     <label {...methods.getLabelProps()}>
@@ -27,14 +43,41 @@ const MockRadio: FC<{}> = () => {
   )
 }
 
-test('should have enum items', () => {
+test('should have string enum items', () => {
   const { getByText } = render(
     <FormContext schema={mockRadioSchema}>
-      <MockRadio />
+      <MockRadio path="#/stringTest" />
     </FormContext>
   )
 
-  for (const item of mockRadioSchema.enum) {
+  for (const item of mockRadioSchema.properties.stringTest.enum) {
     expect(getByText(item)).toBeDefined()
   }
+})
+
+test('should have all integers in interval', () => {
+  const { getByText } = render(
+    <FormContext schema={mockRadioSchema}>
+      <MockRadio path="#/integerTest" />
+    </FormContext>
+  )
+
+  for (
+    let i = mockRadioSchema.properties.integerTest.minimum;
+    i < mockRadioSchema.properties.integerTest.maximum;
+    i += mockRadioSchema.properties.integerTest.multipleOf
+  ) {
+    expect(getByText(i.toString())).toBeDefined()
+  }
+})
+
+test('should have boolean true and false', () => {
+  const { getByText } = render(
+    <FormContext schema={mockRadioSchema}>
+      <MockRadio path="#/booleanTest" />
+    </FormContext>
+  )
+
+  expect(getByText('true')).toBeDefined()
+  expect(getByText('false')).toBeDefined()
 })
