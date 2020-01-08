d072fbf9290784e6ba8237aebcdbf95d9defa627
Helena Steck <helena.steck@vtex.com.br>
2020-01-08 15:57:42 -0300

    Added return options for input and labels of each item

--- src/hooks/types/index.ts
+++ src/hooks/types/index.ts
@@ -4,6 +4,7 @@ export interface UseRadioParameters {
   (path: string): {
     getLabelProps(): React.ComponentProps<'label'>
     getItems(): Array<string>
-    getItemProps(index: number): React.ComponentProps<'input'>
+    getItemInputProps(index: number): React.ComponentProps<'input'>
+    getItemLabelProps(index: number): React.ComponentProps<'label'>
   }
 }
