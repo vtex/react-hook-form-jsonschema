e0084eea54f2466aff88c6f6dbca1a427b875b82
Helena Steck <helena.steck@vtex.com.br>
2020-01-14 11:42:43 -0300

    Fixed not assigning concat values to the inputs array, changed returns inside the switch to a push to the inputs array, for better readability

--- src/hooks/useObject.ts
+++ src/hooks/useObject.ts
@@ -11,48 +11,52 @@ import { getInputCustomFields } from './useInput'
 import { getRadioCustomFields } from './useRadio'
 import { useFormContext } from '../components/types'
 
-const useObjectStructure = (path: string): UseObjectReturnType => {
-  function getStructure(
-    formContext: FormContextValues,
-    pathInfo: [JSONSchemaType, boolean, string],
-    path: string
-  ): UseObjectReturnType {
-    const inputs: UseObjectReturnType = []
-    const currentObject = pathInfo[0]
+function getStructure(
+  formContext: FormContextValues,
+  pathInfo: [JSONSchemaType, boolean, string],
+  path: string
+): UseObjectReturnType {
+  let inputs: UseObjectReturnType = []
+  const currentObject = pathInfo[0]
 
-    const genericInput = getGenericInput(formContext, pathInfo, path)
-    switch (currentObject.type) {
-      case 'string':
-        return [getInputCustomFields(genericInput)]
-      case 'integer':
-      case 'number':
-        return [getInputCustomFields(genericInput)]
-      case 'object':
-        {
-          const objKeys = Object.keys(currentObject.properties)
-          const requiredFields: Array<string> | undefined =
-            currentObject.required
-          for (const key of objKeys) {
-            const isRequired = requiredFields
-              ? requiredFields.indexOf(key) !== -1
-              : false
-            const currentPathInfo: [JSONSchemaType, boolean, string] = [
-              currentObject[key],
-              isRequired,
-              key,
-            ]
-            const currentPath = concatFormPath(path, key)
-            inputs.concat(
-              getStructure(formContext, currentPathInfo, currentPath)
-            )
-          }
+  const genericInput = getGenericInput(formContext, pathInfo, path)
+  switch (currentObject.type) {
+    case 'string':
+      inputs.push(getInputCustomFields(genericInput))
+      break
+    case 'integer':
+    case 'number':
+      inputs.push(getInputCustomFields(genericInput))
+      break
+    case 'boolean':
+      inputs.push(getRadioCustomFields(genericInput))
+      break
+    case 'object':
+      {
+        const objKeys = Object.keys(currentObject.properties)
+        const requiredFields: Array<string> | undefined = currentObject.required
+        for (const key of objKeys) {
+          const isRequired = requiredFields
+            ? requiredFields.indexOf(key) !== -1
+            : false
+          const currentPathInfo: [JSONSchemaType, boolean, string] = [
+            currentObject.properties[key],
+            isRequired,
+            key,
+          ]
+          const currentPath = concatFormPath(path, key)
+          inputs = inputs.concat(
+            getStructure(formContext, currentPathInfo, currentPath)
+          )
         }
-        break
-    }
-
-    return inputs
+      }
+      break
   }
 
+  return inputs
+}
+
+export const useObject: UseObjectProperties = path => {
   const childArray = getStructure(
     useFormContext(),
     useObjectFromPath(path),
@@ -60,8 +64,4 @@ const useObjectStructure = (path: string): UseObjectReturnType => {
   )
 
   return childArray
-}
-
-export const useObject: UseObjectProperties = path => {
-  return useObjectStructure(path)
 }
