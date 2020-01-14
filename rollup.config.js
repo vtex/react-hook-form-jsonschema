2c4f544ab5b97ebd696cba72953e321f5bdc26f0
Helena Steck <helena.steck@vtex.com.br>
2020-01-14 16:53:53 -0300

    Added commonjs plugin to resolve react named exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
+import react from 'react'
+import reactDom from 'react-dom'
 
 export default {
   input: 'src/index.ts',
@@ -7,5 +10,15 @@ export default {
     dir: 'output',
     format: 'cjs',
   },
-  plugins: [resolve(), typescript()],
+  plugins: [
+    resolve(),
+    typescript(),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        react: Object.keys(react),
+        'react-dom': Object.keys(reactDom),
+      },
+    }),
+  ],
 }
