9941433d4572cbf5b55196ccd1959acbf4e539f6
Helena Steck <helena.steck@vtex.com.br>
2020-01-17 13:37:20 -0300

    Formatting

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
+
 import pkg from './package.json'
 
 const PROD = !process.env.ROLLUP_WATCH
