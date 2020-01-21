68bdcf2bd336a519a74e559f18acd65f8acdc6bb
Helena Steck <helena.steck@vtex.com.br>
2020-01-21 14:53:28 -0300

    Add comments to clarify the path handling

--- src/JSONSchema/path-handler.ts
+++ src/JSONSchema/path-handler.ts
@@ -7,9 +7,13 @@ const concatFormPath = (path: string, newNode: string): string => {
 
 const getSplitPath = (path: string): Array<string> => {
   const split = path.split('/')
+  // Removes the root path if it is present(it should always be, but if it
+  // isn't this can prevent some errors)
   if (split[0] === '#') {
     split.shift()
   }
+  // If there was a '/' at the end of the path before it is split(it should not
+  // be there) this removes the non-existent path
   if (split[split.length - 1] === '') {
     split.pop()
   }
