c6f7feb99da3769cce65a31e78bdc11b39690c3c
Helena Steck <helena.steck@vtex.com.br>
2020-01-21 15:32:35 -0300

    Change now skips loop iteration on edge case first

--- src/JSONSchema/path-handler.ts
+++ src/JSONSchema/path-handler.ts
@@ -64,50 +64,51 @@ const getObjectFromForm = (
 
   for (const key of orderedSchemaKeys) {
     const splitPath = getSplitPath(key)
+    if (!splitPath) {
+      continue
+    }
 
     let current = objectFromData
     let currentOriginal = originalSchema
 
-    if (splitPath) {
-      for (let node = 0; node < splitPath.length; node++) {
-        if (currentOriginal === undefined) {
-          break
-        }
-
-        if (currentOriginal.type && currentOriginal.type === 'object') {
-          currentOriginal = currentOriginal.properties
-        } else if (
-          !currentOriginal.type &&
-          currentOriginal[splitPath[node]] &&
-          currentOriginal[splitPath[node]].type === 'object'
-        ) {
-          currentOriginal = currentOriginal[splitPath[node]]
-        }
-
-        if (node === splitPath.length - 1) {
-          if (
-            currentOriginal[splitPath[node]] &&
-            currentOriginal[splitPath[node]].type &&
-            currentOriginal[splitPath[node]].type === 'integer'
-          ) {
-            current[splitPath[node]] = parseInt(data[key])
-          } else if (
-            currentOriginal[splitPath[node]] &&
-            currentOriginal[splitPath[node]].type &&
-            currentOriginal[splitPath[node]].type === 'number'
-          ) {
-            current[splitPath[node]] = parseFloat(data[key])
-          } else {
-            current[splitPath[node]] = data[key]
-          }
-        }
-
-        if (current[splitPath[node]] === undefined) {
-          current[splitPath[node]] = {}
-        }
-
-        current = current[splitPath[node]]
+    for (let node = 0; node < splitPath.length; node++) {
+      if (currentOriginal === undefined) {
+        break
       }
+
+      if (currentOriginal.type && currentOriginal.type === 'object') {
+        currentOriginal = currentOriginal.properties
+      } else if (
+        !currentOriginal.type &&
+        currentOriginal[splitPath[node]] &&
+        currentOriginal[splitPath[node]].type === 'object'
+      ) {
+        currentOriginal = currentOriginal[splitPath[node]]
+      }
+
+      if (node === splitPath.length - 1) {
+        if (
+          currentOriginal[splitPath[node]] &&
+          currentOriginal[splitPath[node]].type &&
+          currentOriginal[splitPath[node]].type === 'integer'
+        ) {
+          current[splitPath[node]] = parseInt(data[key])
+        } else if (
+          currentOriginal[splitPath[node]] &&
+          currentOriginal[splitPath[node]].type &&
+          currentOriginal[splitPath[node]].type === 'number'
+        ) {
+          current[splitPath[node]] = parseFloat(data[key])
+        } else {
+          current[splitPath[node]] = data[key]
+        }
+      }
+
+      if (current[splitPath[node]] === undefined) {
+        current[splitPath[node]] = {}
+      }
+
+      current = current[splitPath[node]]
     }
   }
 
