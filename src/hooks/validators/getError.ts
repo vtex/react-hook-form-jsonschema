8b031ca83c0ee95ade2d3edbb01614cea27f70c2
Helena Steck <helena.steck@vtex.com.br>
2020-01-21 16:11:02 -0300

    Change function body to return undefined first if errors is not defined, it makes the code more clean

--- src/hooks/validators/getError.ts
+++ src/hooks/validators/getError.ts
@@ -11,43 +11,44 @@ export const getError = (
   maximum?: number,
   step?: number | 'any'
 ) => {
-  if (errors) {
-    const retError: ErrorMessage = {
-      message: ErrorTypes.undefinedError,
-      expected: undefined,
-    }
-    switch (errors.message) {
-      case ErrorTypes.required:
-        retError.message = ErrorTypes.required
-        retError.expected = isRequired
-        break
-      case ErrorTypes.maxLength:
-        retError.message = ErrorTypes.maxLength
-        retError.expected = currentObject.maxLength
-        break
-      case ErrorTypes.minLength:
-        retError.message = ErrorTypes.minLength
-        retError.expected = currentObject.minLength
-        break
-      case ErrorTypes.maxValue:
-        retError.message = ErrorTypes.maxValue
-        retError.expected = minimum
-        break
-      case ErrorTypes.minValue:
-        retError.message = ErrorTypes.minValue
-        retError.expected = maximum
-        break
-      case ErrorTypes.multipleOf:
-        retError.message = ErrorTypes.multipleOf
-        retError.expected = step
-        break
-      case ErrorTypes.pattern:
-        retError.message = ErrorTypes.pattern
-        retError.expected = currentObject.pattern
-        break
-    }
-    return retError
-  } else {
+  if (!errors) {
     return undefined
   }
+
+  const retError: ErrorMessage = {
+    message: ErrorTypes.undefinedError,
+    expected: undefined,
+  }
+
+  switch (errors.message) {
+    case ErrorTypes.required:
+      retError.message = ErrorTypes.required
+      retError.expected = isRequired
+      break
+    case ErrorTypes.maxLength:
+      retError.message = ErrorTypes.maxLength
+      retError.expected = currentObject.maxLength
+      break
+    case ErrorTypes.minLength:
+      retError.message = ErrorTypes.minLength
+      retError.expected = currentObject.minLength
+      break
+    case ErrorTypes.maxValue:
+      retError.message = ErrorTypes.maxValue
+      retError.expected = minimum
+      break
+    case ErrorTypes.minValue:
+      retError.message = ErrorTypes.minValue
+      retError.expected = maximum
+      break
+    case ErrorTypes.multipleOf:
+      retError.message = ErrorTypes.multipleOf
+      retError.expected = step
+      break
+    case ErrorTypes.pattern:
+      retError.message = ErrorTypes.pattern
+      retError.expected = currentObject.pattern
+      break
+  }
+  return retError
 }
