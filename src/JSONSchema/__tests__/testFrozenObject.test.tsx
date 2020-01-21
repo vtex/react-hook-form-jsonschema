7a0dc73384c8bd88fa61159eddb1792c63c3e4e1
Helena Steck <helena.steck@vtex.com.br>
2020-01-21 15:04:10 -0300

    Add test to check if schema is not modified

---
+++ src/JSONSchema/__tests__/testFrozenObject.test.tsx
@@ -0,0 +1,141 @@
+import React, { FC } from 'react'
+import { render, wait } from '@vtex/test-tools/react'
+
+import { FormContext } from '../../components'
+import {
+  UseRawInputReturnType,
+  InputReturnTypes,
+  InputTypes,
+  UseRadioReturnType,
+  UseSelectReturnType,
+  useObject,
+  UISchemaType,
+  UITypes,
+} from '../../hooks'
+import mockObjectSchema, { toFixed } from '../../hooks/__mocks__/mockSchema'
+
+const mockUISchema: UISchemaType = {
+  type: UITypes.default,
+  properties: {
+    numberTest: {
+      type: UITypes.select,
+    },
+  },
+}
+
+const SpecializedObject: FC<{ baseObject: InputReturnTypes }> = props => {
+  switch (props.baseObject.type) {
+    case InputTypes.input: {
+      const inputObject = props.baseObject as UseRawInputReturnType
+      return (
+        <>
+          <label {...inputObject.getLabelProps()}>{inputObject.name}</label>
+          <input {...inputObject.getInputProps()} />
+        </>
+      )
+    }
+    case InputTypes.radio: {
+      const radioObject = props.baseObject as UseRadioReturnType
+      return (
+        <>
+          <label {...radioObject.getLabelProps()}>{radioObject.name}</label>
+          {radioObject.getItems().map((value, index) => {
+            return (
+              <label
+                {...radioObject.getItemLabelProps(index)}
+                key={`${value}${index}`}
+              >
+                {value}
+                <input {...radioObject.getItemInputProps(index)} />
+              </label>
+            )
+          })}
+        </>
+      )
+    }
+    case InputTypes.select: {
+      const selectObject = props.baseObject as UseSelectReturnType
+      return (
+        <>
+          <label {...selectObject.getLabelProps()}>{selectObject.name}</label>
+          <select {...selectObject.getSelectProps()}>
+            {selectObject.getItems().map((value, index) => {
+              return (
+                <option
+                  {...selectObject.getItemOptionProps(index)}
+                  key={`${value}${index}`}
+                >
+                  {value}
+                </option>
+              )
+            })}
+          </select>
+        </>
+      )
+    }
+  }
+  return <></>
+}
+
+const MockObject: FC<{ path: string; UISchema?: UISchemaType }> = props => {
+  const methods = useObject({ path: props.path, UISchema: props.UISchema })
+
+  const objectForm = []
+  for (const obj of methods) {
+    objectForm.push(
+      <div key={`${obj.type}${obj.path}`}>
+        <SpecializedObject baseObject={obj} />
+        {obj.getError() && <p>This is an error!</p>}
+      </div>
+    )
+  }
+
+  return <>{objectForm}</>
+}
+
+test('should render all child properties of the schema', () => {
+  const { getByText } = render(
+    <FormContext schema={Object.freeze(mockObjectSchema)}>
+      <MockObject path="#" />
+    </FormContext>
+  )
+
+  for (const item of Object.keys(mockObjectSchema.properties)) {
+    expect(getByText(item)).toBeDefined()
+  }
+})
+
+test('should raise error', async () => {
+  const { getByText } = render(
+    // esling-disable-next-line no-console
+    <FormContext
+      schema={Object.freeze(mockObjectSchema)}
+      onSubmit={() => {
+        return
+      }}
+    >
+      <MockObject path="#/errorTest" />
+      <input type="submit" value="Submit" />
+    </FormContext>
+  )
+
+  getByText('Submit').click()
+
+  await wait(() => expect(getByText('This is an error!')).toBeDefined())
+})
+
+test('ui schema should render number and input as select', async () => {
+  const { getByText } = render(
+    <FormContext schema={Object.freeze(mockObjectSchema)}>
+      <MockObject path="#" UISchema={mockUISchema} />
+    </FormContext>
+  )
+
+  for (
+    let i = mockObjectSchema.properties.numberTest.minimum;
+    i < mockObjectSchema.properties.numberTest.maximum;
+    i += mockObjectSchema.properties.numberTest.multipleOf
+  ) {
+    expect(getByText(toFixed(i, 1))).toBeDefined()
+  }
+})
