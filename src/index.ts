a6c141ce723268afbbed08eb05675a46c2da74a9
Helena Steck <helena.steck@vtex.com.br>
2020-01-23 13:41:04 -0300

    Export ErrorMessage typings

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ export * from 'react-hook-form'
 export { FormContext, FormContextProps } from './components'
 export * from './hooks'
 export * from './JSONSchema'
+
+export {
+  ErrorMessage,
+  ErrorMessageValues,
+  ErrorTypes,
+} from './hooks/validators/types'
