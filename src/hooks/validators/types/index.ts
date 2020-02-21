697a35078ea24777a3c764d750fb58517cd98c00
Thiago Murakami <thiago.murakami@gmail.com>
2020-02-21 09:44:59 -0300

    Fix typo

--- src/hooks/validators/types/index.ts
+++ src/hooks/validators/types/index.ts
@@ -6,8 +6,8 @@ import {
 
 export enum ErrorTypes {
   required = '__form_error_required__',
-  maxLength = '__form_error_maxLenght__',
-  minLength = '__form_error_minLenght__',
+  maxLength = '__form_error_maxLength__',
+  minLength = '__form_error_minLength__',
   maxValue = '__form_error_maxValue__',
   minValue = '__form_error_minValue__',
   pattern = '__form_error_pattern__',
